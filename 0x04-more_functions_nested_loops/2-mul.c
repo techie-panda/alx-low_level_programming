b4b04e2c8e431b8061e6df0f93756417abb0b252
Laphtain Sithole <173262765+techie-panda@users.noreply.github.com>
2024-06-27 20:57:14 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -11,23 +11,3 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
-
-
-Task 3
-#include "main.h"
-
-/**
- * print_numbers - print 0-9
- * return: void
- */
-
-void print_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-}
